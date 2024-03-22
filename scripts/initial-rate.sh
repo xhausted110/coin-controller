6deb7d7cfb02b8cec974273736ff036f3cc544fd
root <root@9020-asterisk1.incompetel.net>
2024-03-21 22:00:48 -0400

    fixed typo

--- scripts/initial-rate.sh
+++ scripts/initial-rate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tty=/dev/ttyUSB0
 exec 4<$tty 5>$tty #Gotta be honest, I copied this from somewhere. It seems to redirect stuff sent to &5 to the tty, and stuff received to the tty to &4
-stty -F $tty 1200 cs8 -echo -brkint -imaxbel -ixon # set up the serial port. 1200 baud, 8 bit character size, no echo, disable breaks causing an interrupt signal, flush full impact buffer on a character, disable xon/xoff flow control.
+stty -F $tty 1200 cs8 -echo -brkint -imaxbel -ixon # set up the serial port. 1200 baud, 8 bit character size, no echo, disable breaks causing an interrupt signal, flush full input buffer on a character, disable xon/xoff flow control.
 echo 1 >&5 #echo sends a 1 followed by a carriage return to the serial port
 read reply <&4 #read the response into the reply variable and stop listening when we get a newline. These are the echoed characters.
 read -d '?' reply <&4 # read the response into the reply variable, overwriting the above since we don't care about that. stop reading when we get a ?
